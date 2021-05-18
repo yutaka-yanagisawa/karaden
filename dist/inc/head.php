fa23e8480d64d4e4c43a839084ab81981f103d17
柳澤豊 <y-yanagisawa@roudiam.co.jp>
2021-05-19 01:34:38 +0900

    include化

---
+++ dist/inc/head.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head prefix="og: http://ogp.me/ns#">
+  <!-- Google Tag Manager-->
+  <script>
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-0000000');
+  </script>
+  <!-- End Google Tag Manager-->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="format-detection" content="telephone=no">
+  <meta name="keywords" content="SMS,ショートメール,ショートメッセージ,クラウド,ASP,API,代理店,電話番号,携帯番号,NTT,SMS,SMS送信,空電プッシュ,空電,からでん,NTTメディアクロス">
+  <meta name="description" content="SMS送信サービス　空電プッシュ(法人向けASP)は、SMSの一斉配信だけでなく、お客様がご用意されたCTI(電話とコンピューターの統合システム）やIVR（音声自動応答装置）などの顧客管理システムと連携して自動的にSMS送信するなど、用途にあわせて様々な機能を提供します。">
+  <meta property="og:url" content="">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="サービス詳細と空電プッシュの強み | SMS送信サービス 「空電プッシュ」(法人向けASP)">
+  <meta property="og:description" content="">
+  <meta property="og:site_name" content="SMS送信サービス　空電プッシュ(法人向けASP)は、SMSの一斉配信だけでなく、お客様がご用意されたCTI(電話とコンピューターの統合システム）やIVR（音声自動応答装置）などの顧客管理システムと連携して自動的にSMS送信するなど、用途にあわせて様々な機能を提供します。">
+  <meta property="og:image" content="">
+  <link href="assets/css/style.css" rel="stylesheet">
+  <script src="assets/js/jquery-3.6.0.min.js"></script>
+  <script src="assets/js/swiper.min.js"></script>
+  <script src="assets/js/script.js"></script>
+  <script>
+    if ((navigator.userAgent.indexOf('iPhone') > 0) || navigator.userAgent.indexOf('iPod') > 0 || navigator.userAgent.indexOf('Android') > 0) {
+    document.write('<meta name="viewport" content="width=device-width,initial-scale=1.0">');
+    }else{
+    document.write('<meta name="viewport" content="width=1400">');
+    }
+  </script>
+  <script>
+    (function(d) {
+    var config = {
+    kitId: 'yqa3ryc',
+    scriptTimeout: 3000,
+    async: true
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+    })(document);
+  </script>
+  <title>サービス詳細と空電プッシュの強み | SMS送信サービス 「空電プッシュ」(法人向けASP)</title>
+</head>
